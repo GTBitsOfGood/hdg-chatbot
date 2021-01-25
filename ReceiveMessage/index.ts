744fe3f0f5ceabbc9243cc2a49fc616d400a2cff
Matt Chen <matt.chen522@gmail.com>
2021-01-24 19:42:15 -0500

    initial commit

---
+++ ReceiveMessage/index.ts
@@ -0,0 +1,25 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import * as twilio from 'twilio'
+import * as qs from 'qs'
+
+const MessagingResponse = twilio.twiml.MessagingResponse
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    const sentMessage = qs.parse(req.body)
+
+    const message = new MessagingResponse()
+    message.message("you said " + sentMessage.Body)
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: message.toString(),
+        headers: { 'Content-Type': 'application/xml' },
+        isRaw: true
+    };
+
+    context.done()
+
+};
+
+export default httpTrigger;
