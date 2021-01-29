6b212203b96f4e9f1385122218232f9cbb75ea4d
Matt Chen <matt.chen522@gmail.com>
2021-01-28 23:35:42 -0500

    test

--- ReceiveMessage/index.ts
+++ ReceiveMessage/index.ts
@@ -7,7 +7,7 @@ const MessagingResponse = twilio.twiml.MessagingResponse;
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const sentMessage = qs.parse(req.body);
-
+    //hello delete me one day
     const message = new MessagingResponse();
     message.message('you said ' + sentMessage.Body);
 
