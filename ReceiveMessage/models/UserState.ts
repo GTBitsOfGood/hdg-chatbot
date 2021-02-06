09270aa2b9a58c230fe77e44f6f3745ef63a6435
Matt Chen <matt.chen522@gmail.com>
2021-02-06 17:46:15 -0500

    move models into azure function

---
+++ ReceiveMessage/models/UserState.ts
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserStateSchema = new Schema({
+    userId: { type: String, required: true, unique: true },
+    currMessage: { type: { type: Schema.ObjectId, ref: 'ChatbotMessage' }, required: true },
+    split: { type: Boolean, required: false },
+});
+
+module.exports = mongoose.model('UserState', UserStateSchema);
