f204e3b6b3ba74e10236e0e2c20d9679d5d9b31e
Alex Bates <alexb8z@lawn-143-215-112-174.lawn.gatech.edu>
2021-02-06 13:22:21 -0500

    PR comments addressed

--- models/ChatbotMessage.ts
+++ models/ChatbotMessage.ts
@@ -1,10 +1,9 @@
 const ChatbotMessageSchema = new Schema({
-  body: { type: String, required: true },
-  images: { type: [{url: String}], required: false },
-  nextMessages: { type: [{type: Schema.ObjectId, ref: 'ChatbotMessage'}], required: true },
-  previousMessage: { type: {type: Schema.ObjectId, ref: 'ChatbotMessage'}, required: true },
-  module: { type: String, required: true } 
+    body: { type: String, required: true },
+    images: { type: [{ url: String }], required: false },
+    nextMessages: { type: Map, of: { type: Schema.ObjectId, ref: 'ChatbotMessage' }, required: true },
+    previousMessage: { type: { type: Schema.ObjectId, ref: 'ChatbotMessage' }, required: true },
+    module: { type: String, required: true },
 });
-
 
 module.exports = mongoose.model('ChatbotMessage', ChatbotMessageSchema);
