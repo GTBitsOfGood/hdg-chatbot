97b494da002fa9ebcf3eef9e5e2f3f66fbb420f3
Alex Bates <alexb8z@lawn-143-215-112-174.lawn.gatech.edu>
2021-02-23 16:35:30 -0500

    Inserts the userState into the database when a user is new using the userId as the phone number of the phone the message was sent from

--- ReceiveMessage/models/UserState.ts
+++ ReceiveMessage/models/UserState.ts
@@ -1,22 +1,27 @@
 import mongoose, { Schema, Document } from 'mongoose';
-import {IMessage} from './ChatbotMessage';
+import { IMessage } from './ChatbotMessage';
 
 export interface State extends Document {
     userId: string;
     currMessage: IMessage['_id'];
-    completedModules: Array <number>;
+    completedModules: Array<number>;
     split: boolean;
     lowData: boolean;
-    completedTimes: Array <Schema.Types.Date>;
+    completedTimes: Array<Schema.Types.Date>;
 }
 
 const UserStateSchema = new Schema({
     userId: { type: String, required: true, unique: true },
-    currMessage: { type: Schema.Types.ObjectId, ref: 'ChatbotMessage', required: true },
-    completedModules: {type: Array, "default": []},
+    currMessage: {
+        type: Schema.Types.ObjectId,
+        ref: 'ChatbotMessage',
+        required: true,
+        default: '6022178429efc055c8e74e50',
+    },
+    completedModules: { type: Array, default: [] },
     split: { type: Boolean, required: false },
-    lowData: { type: Boolean},
-    completedTimes: {type: Array, "default": [null, null, null, null, null]},
+    lowData: { type: Boolean },
+    completedTimes: { type: Array, default: [null, null, null, null, null] },
 });
 
-export default mongoose.model<State>("UserState", UserStateSchema);
+export default mongoose.model<State>('UserState', UserStateSchema);
