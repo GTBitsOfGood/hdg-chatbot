6cbd2867d94c701062d4d6d17176e675d715fa98
sgao88 <serena18188@gmail.com>
2021-02-27 22:35:39 -0500

    initial
    first try at creating new function and special message ids file

--- ReceiveMessage/index.ts
+++ ReceiveMessage/index.ts
@@ -7,6 +7,8 @@ import UserState from './models/UserState';
 import { Schema } from 'mongoose';
 import formResponse from './Scripts/sendMessage';
 import { ConferenceContext } from 'twilio/lib/rest/api/v2010/account/conference';
+import specialMessageIds from './specialMessageIds';
+import ChatbotMessage from './models/ChatbotMessage';
 
 const MessagingResponse = twilio.twiml.MessagingResponse;
 
@@ -17,33 +19,50 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const curUserState = await readUserRequest(req);
     context.log(curUserState);
     context.log(sentMessage);
-    const response = await formResponse(curUserState, sentMessage.Body);
+    // const response = await formResponse(curUserState, sentMessage.Body);
+    const response = await manageKeywordSent(sentMessage, curUserState);
 
     const message = new MessagingResponse();
     message.message(response);
 
     // if there's a conditional (like not recording all messages), put that here
     storeMessage(sentMessage, curUserState.currMessage);
-    
+
     context.res = {
         // status: 200, /* Defaults to 200 */ /*
         body: message.toString(),
         headers: { 'Content-Type': 'application/xml' },
         isRaw: true,
     };
-    
+
     context.done();
 };
 
-const storeMessage = async function (sentMessage:qs.ParsedQs, curMessageID: Schema.Types.ObjectId) {
-    const userMessage  = new MessageResponse({
+const storeMessage = async function (sentMessage: qs.ParsedQs, curMessageID: Schema.Types.ObjectId) {
+    const userMessage = new MessageResponse({
         accountID: sentMessage.From,
         chatBotMessageID: curMessageID,
-        response: sentMessage.Body});
+        response: sentMessage.Body,
+    });
     userMessage.save(function (err, mes) {
         if (err) return console.error(err);
-        console.log("Saved message to database");
+        console.log('Saved message to database');
     });
-}
+};
+
+//checks if a special keyword is in the message sent
+const manageKeywordSent = async function (sentMessage: qs.ParsedQs, curUserState) {
+    if (specialMessageIds.has(sentMessage.Body)) {
+        // special handling
+        const responseID = specialMessageIds.get(sentMessage.Body);
+        const responseMessage = await ChatbotMessage.findById([responseID]);
+        const responseString = responseMessage.body;
+        return responseString;
+    } else {
+        // normal handling
+        const responseString = await formResponse(curUserState, sentMessage.Body);
+        return responseString;
+    }
+};
 
 export default httpTrigger;
---
+++ ReceiveMessage/specialMessageIds.ts
@@ -0,0 +1,7 @@
+import ChatbotMessage from '../ReceiveMessage/models/ChatbotMessage';
+
+const specialMessageIds = new Map();
+
+specialMessageIds.set('restart', '6022178429efc055c8e74e50');
+
+export default specialMessageIds;
