eaa0eb04701d0bd514d850b005ca68fc778b607e
Chen Matt <matt.chen522@gmail.com>
2021-03-03 08:31:34 -0500

    prettier

--- migrations/20210223222716-userstate-migrate.js
+++ migrations/20210223222716-userstate-migrate.js
@@ -4,19 +4,17 @@ module.exports = {
         // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
         // Example:
         // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
-        await db.collection('userstates').updateMany({}, { $set: { completedModules: [] } });
-        await db.collection('userstates').updateMany({}, { $set: { completedTimes: [null, null, null, null, null] } });
-        await db.collection('userstates').updateMany({}, { $set: { split: false } });
+        await db.collection('userstates').updateMany({}, { $set: { completedModules: [] } })
+        await db.collection('userstates').updateMany({}, { $set: { completedTimes: [null, null, null, null, null] } })
+        await db.collection('userstates').updateMany({}, { $set: { split: false } })
     },
 
     async down(db) {
         // TODO write the statements to rollback your migration (if possible)
         // Example:
         // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
-        await db.collection('userstates').updateMany({}, { $unset: { completedModules: [] } });
-        await db
-            .collection('userstates')
-            .updateMany({}, { $unset: { completedTimes: [null, null, null, null, null] } });
-        await db.collection('userstates').updateMany({}, { $unset: { split: false } });
+        await db.collection('userstates').updateMany({}, { $unset: { completedModules: [] } })
+        await db.collection('userstates').updateMany({}, { $unset: { completedTimes: [null, null, null, null, null] } })
+        await db.collection('userstates').updateMany({}, { $unset: { split: false } })
     },
-};
+}
