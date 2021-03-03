bf5c1f652977c885bb762a5790825e121c21917f
Chen Matt <matt.chen522@gmail.com>
2021-03-03 08:30:36 -0500

    install package, ignore migrations

--- migrations/20210223222716-userstate-migrate.js
+++ migrations/20210223222716-userstate-migrate.js
@@ -1,20 +1,22 @@
 module.exports = {
-  async up(db, client) {
-    // TODO write your migration here.
-    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
-    await db.collection('userstates').updateMany({}, {$set: {completedModules: []}});
-    await db.collection('userstates').updateMany({}, {$set: {completedTimes: [null, null, null, null, null]}});
-    await db.collection('userstates').updateMany({}, {$set: {split: false}});
-  },
+    async up(db) {
+        // TODO write your migration here.
+        // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+        // Example:
+        // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+        await db.collection('userstates').updateMany({}, { $set: { completedModules: [] } });
+        await db.collection('userstates').updateMany({}, { $set: { completedTimes: [null, null, null, null, null] } });
+        await db.collection('userstates').updateMany({}, { $set: { split: false } });
+    },
 
-  async down(db, client) {
-    // TODO write the statements to rollback your migration (if possible)
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
-    await db.collection('userstates').updateMany({}, {$unset: {completedModules: []}});
-    await db.collection('userstates').updateMany({}, {$unset: {completedTimes: [null, null, null, null, null]}});
-    await db.collection('userstates').updateMany({}, {$unset: {split: false}});
-  }
+    async down(db) {
+        // TODO write the statements to rollback your migration (if possible)
+        // Example:
+        // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+        await db.collection('userstates').updateMany({}, { $unset: { completedModules: [] } });
+        await db
+            .collection('userstates')
+            .updateMany({}, { $unset: { completedTimes: [null, null, null, null, null] } });
+        await db.collection('userstates').updateMany({}, { $unset: { split: false } });
+    },
 };
