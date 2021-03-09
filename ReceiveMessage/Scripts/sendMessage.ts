43bb76361a409653b34cd5b8c8364e12f583f066
Alex Bates <alexb8z@lawn-143-215-112-174.lawn.gatech.edu>
2021-03-09 18:30:26 -0500

    Recent pushes to spring 3 task 3 before tuesday standup

---
+++ ReceiveMessage/Scripts/sendMessage.ts
@@ -0,0 +1,25 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import * as twilio from 'twilio'
+import qs from 'qs'
+import MongoConnect from './db'
+import UserState from '../models/UserState'
+import ChatbotMessage from '../models/ChatbotMessage'
+
+const formResponse = async function (state, message) {
+    const p = state.currMessage
+    if (p == null) {
+        state.currMessage = '6022178429efc055c8e74e50'
+    }
+    await MongoConnect()
+    const prev = await ChatbotMessage.findById(p)
+    const map = prev.nextMessages
+    let nextChatbotMessageId = map.get(prev.isQuestion ? message : 'default')
+    if (nextChatbotMessageId == null) {
+        nextChatbotMessageId = '6022178429efc055c8e74e50' //change this to whatever error message we want to send (for now it is welcome message)
+    }
+    state.currMessage = nextChatbotMessageId
+    const response = await ChatbotMessage.findById(nextChatbotMessageId)
+    return response
+}
+
+export default formResponse
