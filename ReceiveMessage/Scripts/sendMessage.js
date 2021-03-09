86a3864cc8271cf01077b67b179a48321224a419
Matt Chen <matt.chen522@gmail.com>
2021-03-09 18:35:56 -0500

    demoable

--- ReceiveMessage/Scripts/sendMessage.js
+++ ReceiveMessage/Scripts/sendMessage.js
@@ -1,25 +1,26 @@
-import { AzureFunction, Context, HttpRequest } from '@azure/functions';
-import * as twilio from 'twilio';
-import qs from 'qs';
-import MongoConnect from './db';
-import UserState from '../models/UserState';
-import ChatbotMessage from '../models/ChatbotMessage';
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import * as twilio from 'twilio'
+import qs from 'qs'
+import MongoConnect from './db'
+import UserState from '../models/UserState'
+import ChatbotMessage from '../models/ChatbotMessage'
 
 const formResponse = async function (state, message) {
-    const p = state.currMessage;
+    const p = state.currMessage
     if (p == null) {
-        state.currMessage = '6022178429efc055c8e74e50';
+        state.currMessage = '6022178429efc055c8e74e50'
     }
-    await MongoConnect();
-    const prev = await ChatbotMessage.findById(p);
-    const map = prev.nextMessages;
-    let nextChatbotMessageId = map.get(prev.isQuestion ? message : 'default');
+    await MongoConnect()
+    const prev = await ChatbotMessage.findById(p)
+    const map = prev.nextMessages
+    let nextChatbotMessageId = map.get(prev.isQuestion ? message : 'default')
     if (nextChatbotMessageId == null) {
-        nextChatbotMessageId = '6022178429efc055c8e74e50'; //change this to whatever error message we want to send (for now it is welcome message)
+        nextChatbotMessageId = '6022178429efc055c8e74e50' //change this to whatever error message we want to send (for now it is welcome message)
     }
-    state.currMessage = nextChatbotMessageId;
-    const response = await ChatbotMessage.findById(nextChatbotMessageId);
-    return response;
-};
+    state.currMessage = nextChatbotMessageId
+    state.save()
+    const response = await ChatbotMessage.findById(nextChatbotMessageId)
+    return response
+}
 
-export default formResponse;
+export default formResponse
