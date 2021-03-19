bb5376785ab840997256142c8726a1481344223d
sgao88 <serena18188@gmail.com>
2021-03-18 22:50:50 -0400

    better specialMessageIds handlers
    mapped strings to handler functions in specialMessageIds, fixed functionality in index

--- ReceiveMessage/index.ts
+++ ReceiveMessage/index.ts
@@ -20,16 +20,16 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const response = await manageKeywordSent(sentMessage, curUserState)
 
     // broke based on manageKeywordSent functionality
-    // if (response.images != null) {
-    //     response.images.forEach(function (image) {
-    //         const imageResponse = new MessagingResponse()
-    //         const imageMessage = imageResponse.message('')
-    //         imageMessage.media(image)
-    //     })
-    // }
+    if (response.images != null) {
+        response.images.forEach(function (image) {
+            const imageResponse = new MessagingResponse()
+            const imageMessage = imageResponse.message('')
+            imageMessage.media(image)
+        })
+    }
 
     const message = new MessagingResponse()
-    message.message(response)
+    message.message(response.body)
 
     // if there's a conditional (like not recording all messages), put that here
     if (sentMessage.isQuestion) {
@@ -62,25 +62,13 @@ const storeMessage = async function (sentMessage: qs.ParsedQs, curMessageID: Sch
 const manageKeywordSent = async function (sentMessage: qs.ParsedQs, curUserState) {
     if (specialMessageIds.has(sentMessage.Body)) {
         // special message handling
-        const responseString = specialMessageIds.get(sentMessage.Body)
-
-        // update curUserState depending on the specialMessageId
-        if (sentMessage.Body == 'restart') {
-            curUserState.currMessage = '6022178429efc055c8e74e50'
-            await curUserState.save()
-        } else if (sentMessage.Body == 'completed') {
-            // do not update userstate
-            const responseStringCompleted =
-                'You have completed ' + curUserState.moduleCompletionTime.length + ' modules.'
-            return responseStringCompleted
-        }
-
-        // return message text
+        const responseHandler = specialMessageIds.get(sentMessage.Body)
+        const responseString = responseHandler(curUserState)
         return responseString
     } else {
         // normal handling
         const responseString = await formResponse(curUserState, sentMessage.Body)
-        return responseString.body
+        return responseString
     }
 }
 
--- ReceiveMessage/specialMessageIds.ts
+++ ReceiveMessage/specialMessageIds.ts
@@ -1,15 +1,45 @@
-const specialMessageIds = new Map()
+import qs from 'qs'
+import UserState from './models/UserState'
+import ChatbotMessage, { IMessage } from './models/ChatbotMessage'
 
-//restarts progress
-specialMessageIds.set(
-    'restart',
-    '(Welcome Message) What would you like to learn about? 1. (emoji) Exercise - brief description2. (emoji) WASH- brief description3. (emoji) Nutrition-brief description4. (emoji) Maternal Infant Care-brief description5. (emoji) Mental Health- brief description',
-)
+interface templateSpecialMessageHandler {
+    (curUserState: InstanceType<typeof UserState>): Promise<IMessage>
+}
 
-//checking # of completed modules
-specialMessageIds.set('completed', '')
+const restartHandler: templateSpecialMessageHandler = async function (
+    curUserState: InstanceType<typeof UserState>,
+): Promise<IMessage> {
+    curUserState.currMessage = '6022178429efc055c8e74e50'
+    await curUserState.updateOne()
+    const returnMessage = new ChatbotMessage()
+    returnMessage.body =
+        '(Welcome Message) What would you like to learn about? 1. (emoji) Exercise - brief description2. (emoji) WASH- brief description3. (emoji) Nutrition-brief description4. (emoji) Maternal Infant Care-brief description5. (emoji) Mental Health- brief description'
+    return returnMessage
+}
 
-// help message that gives full list of special messages commands
-specialMessageIds.set('commands', 'Here is a list of commands: restart, completed, help')
+const commandsHandler: templateSpecialMessageHandler = async function (
+    curUserState: InstanceType<typeof UserState>,
+): Promise<IMessage> {
+    const returnMessage = new ChatbotMessage()
+    returnMessage.body = 'Here is a list of commands: restart, completed, help'
+    return returnMessage
+}
+
+const completedHandler: templateSpecialMessageHandler = async function (
+    curUserState: InstanceType<typeof UserState>,
+): Promise<IMessage> {
+    const returnMessage = new ChatbotMessage()
+    returnMessage.body = 'You have completed ' + curUserState.moduleCompletionTime.length + ' modules.'
+    return returnMessage
+}
+
+const specialMessageIds: Map<string | qs.ParsedQs | string[] | qs.ParsedQs[], templateSpecialMessageHandler> = new Map<
+    string | qs.ParsedQs | string[] | qs.ParsedQs[],
+    templateSpecialMessageHandler
+>()
+
+specialMessageIds.set('restart', restartHandler)
+specialMessageIds.set('commands', commandsHandler)
+specialMessageIds.set('completed', completedHandler)
 
 export default specialMessageIds
