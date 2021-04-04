2387cda514eb287388cbd773b499444b4e2a4721
sgao88 <serena18188@gmail.com>
2021-04-04 14:15:20 -0400

    updated fixed messages in sendmessage

--- ReceiveMessage/Scripts/sendMessage.js
+++ ReceiveMessage/Scripts/sendMessage.js
@@ -4,15 +4,19 @@ import fixedMessages from '../fixedMessages'
 
 const formResponse = async function (state, message) {
     const p = state.currMessage
+    // if (p == null) {
+    //     state.currMessage = '6022178429efc055c8e74e50'
+    // }
     if (p == null) {
-        state.currMessage = '6022178429efc055c8e74e50'
+        state.currMessage = fixedMessages.get('welcome')
     }
     await MongoConnect()
     const prev = await ChatbotMessage.findById(p)
     const map = prev.nextMessages
     let nextChatbotMessageId = map.get(map.size > 1 ? message : 'default')
     if (nextChatbotMessageId == null) {
-        nextChatbotMessageId = '6022178429efc055c8e74e50' //change this to whatever error message we want to send (for now it is welcome message)
+        // nextChatbotMessageId = '6022178429efc055c8e74e50'
+        nextChatbotMessageId = fixedMessages.get('error message')
     }
     state.currMessage = nextChatbotMessageId
     state.save()
