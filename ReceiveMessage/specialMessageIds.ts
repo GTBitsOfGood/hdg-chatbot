dac2cc8846631336233d6c0011e12efebe371b3f
Chen Matt <matt.chen522@gmail.com>
2021-04-05 14:27:06 -0400

    delete duplicate import

--- ReceiveMessage/specialMessageIds.ts
+++ ReceiveMessage/specialMessageIds.ts
@@ -2,8 +2,6 @@ import qs from 'qs'
 import ChatbotMessage, { IMessage } from './models/ChatbotMessage'
 import UserState, { IUserState } from './models/UserState'
 import fixedMessages from './fixedMessages'
-import Mongoose from 'mongoose'
-import fixedMessages from './fixedMessages'
 
 interface templateSpecialMessageHandler {
     (curUserState: IUserState): Promise<IMessage>
@@ -13,11 +11,11 @@ const restartHandler: templateSpecialMessageHandler = async function (curUserSta
     curUserState.currMessage = (await fixedMessages.get('Welcome'))._id
     await curUserState.save()
 
-    return fixedMessages.get('welcome');
+    return fixedMessages.get('welcome')
 }
 
 const commandsHandler: templateSpecialMessageHandler = async function (curUserState: IUserState): Promise<IMessage> {
-    return fixedMessages.get('commands');
+    return fixedMessages.get('commands')
 }
 
 const helpHandler: templateSpecialMessageHandler = async function (curUserState: IUserState): Promise<IMessage> {
@@ -41,32 +39,32 @@ const currentHandler: templateSpecialMessageHandler = async function (curUserSta
     return ChatbotMessage.findById(curUserState.currMessage)
 }
 
-const yesDataHandler: templateSpecialMessageHandler = async function (curUserState:IUserState): Promise<IMessage> {
-    curUserState.dataConsent = true;
+const yesDataHandler: templateSpecialMessageHandler = async function (curUserState: IUserState): Promise<IMessage> {
+    curUserState.dataConsent = true
     await curUserState.save()
 
-    return fixedMessages.get('yesDataCollection');
+    return fixedMessages.get('yesDataCollection')
 }
 
-const noDataHandler: templateSpecialMessageHandler = async function (curUserState:IUserState): Promise<IMessage> {
-    curUserState.dataConsent = false;
+const noDataHandler: templateSpecialMessageHandler = async function (curUserState: IUserState): Promise<IMessage> {
+    curUserState.dataConsent = false
     await curUserState.save()
 
-    return fixedMessages.get('noDataCollection');
+    return fixedMessages.get('noDataCollection')
 }
 
-const yesLowDataHandler: templateSpecialMessageHandler = async function (curUserState:IUserState): Promise<IMessage> {
-    curUserState.lowData = true;
+const yesLowDataHandler: templateSpecialMessageHandler = async function (curUserState: IUserState): Promise<IMessage> {
+    curUserState.lowData = true
     await curUserState.save()
 
-    return fixedMessages.get('TurnOnLowData');
+    return fixedMessages.get('TurnOnLowData')
 }
 
-const noLowDataHandler: templateSpecialMessageHandler = async function (curUserState:IUserState): Promise<IMessage> {
-    curUserState.lowData = false;
+const noLowDataHandler: templateSpecialMessageHandler = async function (curUserState: IUserState): Promise<IMessage> {
+    curUserState.lowData = false
     await curUserState.save()
 
-    return fixedMessages.get('TurnOffLowData');
+    return fixedMessages.get('TurnOffLowData')
 }
 
 const deleteHandler: templateSpecialMessageHandler = async function (curUserState: IUserState): Promise<IMessage> {
