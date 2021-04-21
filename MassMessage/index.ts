d2c14747746cf8ab2427f2530bab5cbaef7238ef
Chen Matt <matt.chen522@gmail.com>
2021-04-21 15:37:52 -0400

    haven't tested

--- MassMessage/index.ts
+++ MassMessage/index.ts
@@ -25,10 +25,19 @@ const timerTrigger: AzureFunction = async function (context: Context, myTimer: a
     curDate.setMilliseconds(0)
     // passing this date around so we don't need to repeatedly set times to 0
 
-    const inactive = await messageInactiveUsers(curDate)
-    context.log(inactive)
     const completed = await messageCompletedUsers(curDate)
-    context.log(completed)
+    const inactive = await messageInactiveUsers(curDate)
+    inactive.forEach(async (user) => {
+        if (completed.has(user.userId)) return
+        sendInactiveMessage(user.userId)
+        //hardcoded to welcome message
+        user.currMessage = (await fixedMessages.get('welcome'))._id
+        user.save((err) => {
+            if (err) {
+                console.error(err)
+            }
+        })
+    })
 }
 
 //gets users where last activity was exactly 2 weeks ago
@@ -45,16 +54,6 @@ const messageInactiveUsers = async function (date: Date) {
             $lt: nextDay,
             $gte: twoWeeks,
         },
-    })
-    allUsers.forEach(async (user) => {
-        sendInactiveMessage(user.userId)
-        //hardcoded to welcome message
-        user.currMessage = (await fixedMessages.get('welcome'))._id
-        user.save((err) => {
-            if (err) {
-                console.error(err)
-            }
-        })
     })
     return allUsers
 }
@@ -77,23 +76,33 @@ const messageCompletedUsers = async function (date: Date) {
         },
     })
 
-    allUsers.forEach((user) => {
-        const modules = user.moduleCompletionTime
-            .map((time, index) => {
-                if (time >= twoMonths && time < nextDay) {
-                    // module was completed 2 months ago
-                    return index
-                } else {
-                    return undefined
-                }
-            })
-            .filter((x) => {
-                // undefined is falsy
-                // 0 is also falsy so we need the second check to make sure any match at index 0 goes through
-                return x || x >= 0
-            })
+    const completedSet = new Set<string>()
 
-        sendCompletedMessage(user, user.userId, twoMonths.toDateString(), modules)
+    allUsers.forEach((user) => {
+        let module = -1
+        let found = false
+        let daysIncrement = 1
+        for (let i = 0; i < user.moduleCompletionTime.length; i++) {
+            const time = user.moduleCompletionTime[i]
+            if (time >= twoMonths && time < nextDay) {
+                if (found) {
+                    time.setDate(time.getDate() + daysIncrement)
+                    daysIncrement += 1
+                    user.moduleCompletionTime.set(i, time)
+                } else {
+                    module = i
+                    found = true
+                }
+            }
+        }
+
+        sendCompletedMessage(user, user.userId, twoMonths.toDateString(), module)
+        completedSet.add(user.userId)
+        user.save((err) => {
+            if (err) {
+                console.error(err)
+            }
+        })
 
         /*
         modules.forEach(mod => {
@@ -114,7 +123,7 @@ const messageCompletedUsers = async function (date: Date) {
         */
     })
 
-    return allUsers
+    return completedSet
 }
 
 export default timerTrigger
